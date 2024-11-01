efb0c401b967c0d0a944f7d739e70aac079e9631
zhenzhao86 <zhenzhaobay@gmail.com>
2024-11-01 10:52:47 +0800

    Add files via upload

--- methodology.py
+++ methodology.py
@@ -5,7 +5,6 @@ def methodology():
     st.write("""
     ## Data Flows
     1. **General Query on HDB**: The user's query is passed to the LLM, which processes it and generates a response based on the model’s knowledge base.
-             
     2  **Housing Affordability Calculator**: User inputs household income, savings, other financial details, as well as his desired housing type / town. The inputs are passed to the LLM with advice on whether it is affordable.
 
     ## Flowcharts
@@ -14,3 +13,8 @@ def methodology():
 
     st.image("flowcharts/use_case_1_flowchart.png", caption="Use Case 1: General Query on HDB")
     st.image("flowcharts/use_case_2_flowchart.png", caption="Use Case 2 Housing Affordability Calculator:")
+
+
+    ## Objectives
+    - To allow users to ask general questions about HDB resale prices (based on resale flat prices) and received LLM powered responses.
+    - To provide users with an easy way to calculate their housing affordability, based on their current income/savings and desired house. The LLM would also provide personalized advice based on the input.
