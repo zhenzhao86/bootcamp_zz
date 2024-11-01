fef7c0ea1241fce0e97a41c230961be469c10d6e
zhenzhao86 <zhenzhaobay@gmail.com>
2024-11-01 11:21:29 +0800

    Add files via upload

--- about_us.py
+++ about_us.py
@@ -4,7 +4,7 @@ def about_us():
     st.title("About Us")
     st.write("""
     ## Project Scope
-    This is my first Streamlit and LLM project. This web application serves as an educational tool to help users navigate the HDB resale market, offering two main features: a general query assistant and a housing affordability calculator. 
+    This is my first Streamlit and LLM project. This web application serves as a tool to help users navigate the HDB resale market, offering two main features: a general query assistant and a housing affordability calculator. 
     
     ## Objectives
     - To allow users to ask general questions about HDB resale prices (based on resale flat prices) and received LLM powered responses.
@@ -16,6 +16,12 @@ def about_us():
     ## Features
     - LLM-powered general query assistant (general query)
     - Housing affordability calculator based on income and savings (affordability calculator)
+
+    ## Pages             
+    Affordability Calculator - Input your current finances and desired home, and our LLM will provide personalized advice to afford your dream home.
+    General Query on HDB - Ask questions about HDB resale data.
+    About Us - About this project.
+    Methodology - The logic behind our AI-powered advisors.
     """)
 
     st.image("image/merlion.jpg", caption="Merlion")
